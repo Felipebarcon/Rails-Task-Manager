af571157b56d70a2f775e6453441072ec4d8eaa1
Felipe Barcon <felipe.barcon@gmail.com>
2022-02-05 15:50:41 +0100

    CRUD ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :tasks
+
+#   get "tasks", to: "tasks#index"
+#   get "tasks/new", to: "tasks#new", as: :new_task
+#   post "tasks", to: "tasks#create"
+#   get "tasks/:id", to: "tasks#show", as: :task
+#   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+#   patch "tasks/:id", to: "tasks#update"
+#   delete "task/:id", to: "tasks#destroy"
+ end
